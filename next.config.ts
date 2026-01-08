5508d055a0813f758ff66b02b5935ecf560bd0bc
Kaya <kaya@zlto.co>
2026-01-08 04:50:03 +0200

    mp4 deployment

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,10 @@ const nextConfig: NextConfig = {
             key: 'Cache-Control',
             value: 'public, max-age=31536000, immutable',
           },
+          {
+            key: 'Content-Type',
+            value: 'video/mp4',
+          },
         ],
       },
       {
